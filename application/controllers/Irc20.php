b03ce667333258f62dd53fce6544b816c14c4182
jimmyoz <12707306@qq.com>
2022-01-20 17:00:34 +0800

    Version update

---
+++ application/controllers/Irc20.php
@@ -0,0 +1,294 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require_once(APPPATH.'libraries/Transaction.php');
+use Web3p\EthereumTx\Transaction;
+class Irc20 extends MY_Controller {
+    
+        public function __construct()
+        {
+            parent::__construct();
+            $this->load->model('ette_model');
+            $this->load->model('ifi_coin_model');
+            $this->rpc_url = $this->config->item('ifiRPC');
+            $this->coin_name = "ifi";
+            $this->coin_id = $this->config->item('ifi_coin_id');
+            $this->chain_id = $this->config->item('ifi_chain_id');
+        }
+        
+        
+        public function get_token_balance($address,$contract,$dec){
+            if(substr($address,0,2)=="0x"){
+                $address=substr($address,2);
+            } 
+            $funcSelector = "0x70a08231";
+            $data = $funcSelector . "000000000000000000000000" . $address;
+            $method  = "eth_call";
+            $param1 = [
+                "data"  => $data,
+                "to"    =>  $contract
+            ];
+            $params  = [$param1,"latest"];
+            $result = $this->call($method,$params);
+            return (hexdec($result)/$dec);
+        }
+
+        public function get_nonce($address) {
+            $method  = "eth_getTransactionCount";
+            $param = [$address,"latest"];
+            $result = $this->call($method,$param);
+            if(is_array($result)){
+                exit("\r\n error when getting nonce \r\n");
+            }
+            $count = hexdec($result);
+            return $count;
+        }
+        
+        public function send_ifi()
+        {
+            $to = $_GET['address'];
+            $amount = isset($_GET['amount'])?$_GET['amount'] : 9;
+            $from = $this->config->item("ifiPayAccount");
+            $fromPri = decrypt($this->config->item("encrypted_ifi_wallet"));
+            $contract = $this->config->item("ifi_contract_address");
+            $tx_res = $this->send_token($this->add_random($amount),$from,$fromPri,$contract,$to);
+            echo "\r\n send ifi sucessfully with tx hash : ".$tx_res."\r\n";
+        }
+
+        public function get_ifi()
+        {
+            $input_data = json_decode(trim(file_get_contents('php://input')), true);
+            $owner_address = $input_data['owner_address'];
+            $cpu_name = $input_data['cpu_name'];
+            $cpu_score = $input_data['cpu_score'];
+            $local_ip = $input_data['local_ip'];
+            $data = array(
+                'owner_address' =>  $owner_address,
+                'cpu_name'    =>  $cpu_name,
+                'cpu_score' =>  $cpu_score,
+                'local_ip'  =>  $local_ip,
+                'last_updated' => date('Y-m-d H:i:s')
+            );
+            $this->ette_model->set_node($data, $owner_address);
+            // send ifi
+            $from = $this->config->item("ifiPayAccount");
+            $fromPri = decrypt($this->config->item("encrypted_ifi_wallet"));
+            $contract = $this->config->item("ifi_contract_address");
+            $ifi_amount = $this->add_random($cpu_score);
+            //send ifi to 5 different account
+            // $tx_res1 = $this->send_token($this->cal($ifi_amount,10,100),$from,$fromPri,$contract,$this->config->item("a_address"));
+            // $tx_res2 = $this->send_token($this->cal($ifi_amount,5,100),$from,$fromPri,$contract,$this->config->item("b_address"));
+            // $tx_res3 = $this->send_token($this->cal($ifi_amount,5,100),$from,$fromPri,$contract,$this->config->item("c_address"));
+            // $tx_res4 = $this->send_token($this->cal($ifi_amount,20,100),$from,$fromPri,$contract,$this->config->item("d_address"));
+            $tx_res = $this->send_token($this->cal($ifi_amount,60,100),$from,$fromPri,$contract,$owner_address);
+            if(is_array($tx_res)){
+                echo "\r\n send ifi failed with error : ".json_encode($tx_res,true)."\r\n";
+            } else {
+                $data = array(
+                    'node_address'  =>  $owner_address,
+                    'ifi_amount'    =>  base_convert($ifi_amount,16,10),
+                    'timestamp'     =>  time(),
+                    'from_account'  =>  $from,
+                    'tx_hash'       =>  $tx_res
+                );
+                $this->ette_model->insert_award_log($data);
+                echo "\r\n send ifi sucessfully with tx hash : ".$tx_res."\r\n";
+            }
+        }
+
+        //store the incentive_reward in db
+        public function set_incentive_reward()
+        {
+            $input_data = json_decode(trim(file_get_contents('php://input')), true);
+            $owner_address = $input_data['owner_address'];
+            $ifi_amount = $input_data['ifi_amount'];
+            $tx_res = $input_data['tx_res'];
+            $from_account = $input_data['from_account'];
+
+            $data = array(
+                'node_address'  =>  $owner_address,
+                'ifi_amount'    =>  $ifi_amount,
+                'timestamp'     =>  time(),
+                'from_account'  =>  $from_account,
+                'type'          =>  1,
+                'tx_hash'       =>  $tx_res
+            );
+            $this->ette_model->insert_award_log($data);
+            //send ifi to 4 different account
+            $from = $this->config->item("ifiPayAccount");
+            $fromPri = decrypt($this->config->item("encrypted_ifi_wallet"));
+            $contract = $this->config->item("ifi_contract_address");
+            // $tx_res1 = $this->send_token($this->cal($ifi_amount,10,100),$from,$fromPri,$contract,$this->config->item("a_address"));
+            // $tx_res2 = $this->send_token($this->cal($ifi_amount,5,100),$from,$fromPri,$contract,$this->config->item("b_address"));
+            // $tx_res3 = $this->send_token($this->cal($ifi_amount,5,100),$from,$fromPri,$contract,$this->config->item("c_address"));
+            // $tx_res4 = $this->send_token($this->cal($ifi_amount,20,100),$from,$fromPri,$contract,$this->config->item("d_address"));
+            echo "\r\n set incentive reward sucessfully\r\n";
+        }
+
+        public function register_node()
+        {
+            $input_data = json_decode(trim(file_get_contents('php://input')), true);
+            $owner_address = $input_data['owner_address'];
+            $chequebook_address = $input_data['chequebook_address'];
+            $local_ip = $input_data['local_ip'];
+            $data = array(
+                'owner_address' =>  $owner_address,
+                'chequebook_address'    =>  $chequebook_address,
+                'local_ip'  =>  $local_ip,
+                'last_updated' => date('Y-m-d H:i:s')
+            );
+            $data1 = array(
+                'owner_address' =>  $owner_address,
+                'chequebook_address'    =>  $chequebook_address,
+                'local_ip'  =>  $local_ip,
+                'startup_time' => date('Y-m-d H:i:s')
+            );
+            $this->ette_model->set_node($data, $owner_address);
+            $this->ette_model->insert_node_startup($data1);
+            echo "\r\n register/update the node at the init \r\n";
+        }
+
+        
+
+        private function add_random($amount)
+        {
+            $dec_amount = base_convert($amount,16,10);
+            $new_dec = $dec_amount*(rand(0,20)+100)/100;
+            return base_convert($new_dec,10,16);
+        }
+
+    public function test()
+    {
+        $res = $this->send_token(
+        "9a6df3aabc",
+        "0x0Ab100518367dba7470fE5B2b403387972d453B4",
+        "a2df2ce01d913148bab1aa95d32049227d325db58a0396ae36f88fd1baecd02a",
+        "0x4D2f63d6826603B84D12C1C7dd33aB7F3BDe7553",
+        "0xbed13479c186003fdf2dfc932c3467e7e4431a0e"
+        );
+        echo "\r\n res : ".$res."\r\n";
+    }    
+
+    private function cal($amount,$time,$mul){
+        $amt_dec = base_convert($amount,16,10);
+        $to_dec = $amt_dec*$time/$mul;
+        $to_hex = base_convert($to_dec,10,16);
+        return $to_hex;
+    }
+
+    private function send_token($amount, $from, $privateKey, $contract, $to, $type = 0)
+    {
+
+        if (substr($to, 0, 2) == "0x") {
+            $to = substr($to, 2);
+        }
+        $funcSelector = "0xa9059cbb";
+        // $amt_hex = base_convert($amount,10,16);
+        $amt_hex = $amount;
+        $data = $funcSelector . "000000000000000000000000" . $to;
+
+        if (substr($amt_hex, 0, 2) == "0x") {
+            $amt_hex = substr($amt_hex, 2);
+        }
+        $len = strlen($amt_hex);
+        $amt_val = "";
+        $i = 0;
+        while ($i < 64 - $len) {
+            $amt_val .= "0";
+            $i++;
+        }
+        $amt_val .= $amt_hex;
+        $data .= $amt_val;
+        // $gas = '0x' . dechex(193334);
+        $gas = '0x' . dechex(333334);
+        $gasPrice = '0x' . dechex($this->config->item('gas_price'));
+        if ($type == 1) {
+            $gas = '0x' . dechex(93334);
+        }
+        $nonce = $this->get_nonce($from);
+        $cnonce = '0x' . dechex($nonce);
+        $param = [
+            'nonce'     => $cnonce,
+            'from'      => $from,
+            'to'        => $contract,
+            'gas'       => $gas,
+            'gasPrice'  => $gasPrice,
+            'data'      => $data,
+            'chainId'   =>  $this->config->item('ifi_real_chain_id')
+        ];
+        $transaction = new Transaction($param);
+        $signedTransaction = $transaction->sign($privateKey);
+        $method  = "eth_sendRawTransaction";
+        $params  = ["0x" . $signedTransaction];
+        $out_arr = $this->call($method, $params);
+        return $out_arr;
+    }
+
+        
+        
+        private function toWei($value,$dec) {
+            $float = 0;
+            switch(intval($dec)){
+                case 1:
+                    $float = $value*(1.0E+1);
+                    break;
+                case 2:
+                    $float = $value*(1.0E+2);
+                    break;
+                case 3:
+                    $float = $value*(1.0E+3);
+                    break;
+                case 4:
+                    $float = $value*(1.0E+4);
+                    break;  
+                case 5:
+                    $float = $value*(1.0E+5);
+                    break;
+                case 6:
+                    $float = $value*(1.0E+6);
+                    break;
+                case 7:
+                    $float = $value*(1.0E+7);
+                    break;
+                case 8:
+                    $float = $value*(1.0E+8);
+                    break;
+                case 9:
+                    $float = $value*(1.0E+9);
+                    break;
+                case 10:
+                    $float = $value*(1.0E+10);
+                    break;
+                case 11:
+                    $float = $value*(1.0E+11);
+                    break;
+                case 12:
+                    $float = $value*(1.0E+12);
+                    break;  
+                case 13:
+                    $float = $value*(1.0E+13);
+                    break;
+                case 14:
+                    $float = $value*(1.0E+14);
+                    break;
+                case 15:
+                    $float = $value*(1.0E+15);
+                    break;
+                case 16:
+                    $float = $value*(1.0E+16);
+                    break; 
+                case 17:
+                    $float = $value*(1.0E+17);
+                    break;
+                case 18:
+                    $float = $value*(1.0E+18);
+                    break;
+                default:
+                    $float = $value*(1.0E+1);
+                    break;
+            }
+            return number_format($float,0,'.','');
+        }
+
+}
+
+
---
+++ application/models/Ette_model.php
@@ -0,0 +1,314 @@
+<?php
+
+class Ette_model extends CI_Model {
+    
+    protected $coin_id = 0;
+    protected $cold_wallet = '';
+    public function __construct()
+    {
+        $this->load->database();
+        $this->psql = $this->load->database('ette',true);
+    }
+    
+    
+    public function get_test(){
+        $this->psql->select('*');
+        $this->psql->from('blocks');
+        $this->psql->limit(5);
+        $query = $this->psql->get();
+        return $query->result_array();
+    }
+
+    public function get_wrong_txs(){
+        $this->psql->select('hash');
+        $this->psql->from('transactions');
+        $this->psql->where('from','0x0000000000000000000000000000000000000000');
+        $this->psql->limit(50);
+        $query = $this->psql->get();
+        return $query->result_array();
+    }
+
+    public function get_wrong_bn_txs(){
+        $this->psql->select('hash');
+        $this->psql->from('transactions');
+        $this->psql->where('timestamp',0);
+        $this->psql->limit(250);
+        $query = $this->psql->get();
+        return $query->result_array();
+    }
+
+    public function update_tx_by_hash($data,$where){
+        $this->psql->update('transactions',$data,$where);
+    }
+
+    public function next_check_block() {
+        $this->psql->select('value');
+        $this->psql->where('name','next_check_block');
+        $res = $this->psql->get('config_vars')->row();
+        return $res->value;
+    }
+
+    public function has_block($num) {
+        $this->psql->select('hash');
+        $this->psql->from('blocks');
+        $this->psql->where('number', $num);
+        $query = $this->psql->get();
+        $count = $query->num_rows();
+        if($count > 0) {
+            return $query->row()->hash;
+        } else {
+            return 0;
+        }
+    }
+
+    public function has_tx($hash) {
+        $this->psql->select('hash');
+        $this->psql->from('transactions');
+        $this->psql->where('hash', $hash);
+        $query = $this->psql->get();
+        $count = $query->num_rows();
+        return $count;
+    }
+
+    public function insert_block($data) {
+        $this->psql->insert('blocks', $data);
+    }
+
+    public function insert_transactions($data) {
+        $this->psql->insert('transactions', $data);
+    }
+
+    public function get_trx($max_block,$current_page,$items_per_page=0) {
+        $start = ($current_page-1)*$items_per_page;
+        $this->psql->select('hash,from,to,value,input_data,gas,gasprice,cost,nonce,state,blockhash,blockNumber,timestamp');
+        $this->psql->from('transactions');
+        $this->psql->where('blockNumber <=', $max_block);
+        $data[0] = $this->psql->get()->num_rows();
+        $this->psql->select('hash,from,to,value,input_data,gas,gasprice,cost,nonce,state,blockhash,blockNumber,timestamp');
+        $this->psql->from('transactions');
+        $this->psql->where('blockNumber <=', $max_block);
+        $this->psql->order_by("timestamp", "desc");
+        $this->psql->limit($items_per_page,$start);
+        $query = $this->psql->get();
+        $data[1]    =   $query->result_array();
+        return $data;
+    }
+
+    public function get_signers($current_page=1,$items_per_page=0)
+    {
+        $start = ($current_page-1)*$items_per_page;
+        $this->psql->select('*');
+        $this->psql->from('signers');
+        $data[0]    =   $this->psql->get()->num_rows();
+        $this->psql->select('*');
+        $this->psql->from('signers');
+        if($items_per_page>0) {
+            $this->psql->limit($items_per_page,$start);
+        }
+        $data[1] = $this->psql->get()->result_array();
+        return $data;
+    }
+    public function update_signers($data, $where) {
+        $this->psql->update('signers',$data,$where);
+    }
+
+    public function get_add_trx($current_page=1,$items_per_page=0,$address)
+    {
+        $start = ($current_page-1)*$items_per_page;
+        $this->psql->select('hash');
+        $this->psql->from('transactions');
+        $this->psql->where('from',$address);
+        $this->psql->or_where('to',$address);
+        $data[0]    =   $this->psql->get()->num_rows();
+        $this->psql->order_by("timestamp", "desc");
+        $this->psql->select('hash,blockNumber,timestamp,from,to,state');
+        $this->psql->from('transactions');
+        $this->psql->where('from',$address);
+        $this->psql->or_where('to',$address);
+        if($items_per_page>0){
+            $this->psql->limit($items_per_page,$start);
+        }
+        $data[1] = $this->psql->get()->result_array();
+        return $data;
+    }
+
+
+    public function get_all_blocks($current_page=1,$items_per_page=0)
+    {
+        $start = ($current_page-1)*$items_per_page;
+        $this->psql->select('max(number) as big_block');
+        $this->psql->from('blocks');
+        $this->psql->order_by('time','desc');
+        $data[0]    =  $this->psql->get()->row()->big_block;
+        $this->psql->select('hash,number,time,difficulty,tx_num,size,miner');
+        $this->psql->from('blocks');
+        $this->psql->order_by('time','desc');
+        if($items_per_page>0){
+            $this->psql->limit($items_per_page,$start);
+        }
+        $data[1]    = $this->psql->get()->result_array();
+        return $data;
+    }
+
+    public function get_signed_blocks($current_page=1,$items_per_page=0,$address)
+    {
+        $start = ($current_page-1)*$items_per_page;
+        $this->psql->select('hash,number,time,difficulty,tx_num,size,miner');
+        $this->psql->from('blocks');
+        $this->psql->where('miner',$address);
+        $data[0]    =   $this->psql->get()->num_rows();
+        $this->psql->order_by('time','desc');
+        if($items_per_page>0){
+            $this->psql->limit($items_per_page,$start);
+        }
+        $this->psql->select('hash,number,time,difficulty,tx_num,size,miner');
+        $this->psql->from('blocks');
+        $this->psql->where('miner',$address);
+        $data[1] = $this->psql->get()->result_array();
+        return $data;
+    }
+    // check if address is signer
+    public function check_s($address)
+    {
+        $this->psql->select('id');
+        $this->psql->from('signers');
+        $this->psql->where('address',$address);
+        $count = $this->psql->get()->num_rows();
+        if($count > 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // check if address is common node address
+    public function check_n($address)
+    {
+        $this->psql->select('id');
+        $this->psql->from('nodes');
+        $this->psql->where('owner_address',$address);
+        $this->psql->or_where('chequebook_address',$address);
+        $count = $this->psql->get()->num_rows();
+        if($count > 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function get_nodes($current_page=1,$items_per_page=0)
+    {
+        $start = ($current_page-1)*$items_per_page;
+        $this->psql->select('*');
+        $this->psql->from('nodes');
+        $data[0]    =   $this->psql->get()->num_rows();
+        $this->psql->select('*');
+        $this->psql->from('nodes');
+        if($items_per_page>0){
+            $this->psql->limit($items_per_page,$start);
+        }
+        $data[1] = $this->psql->get()->result_array();
+        return $data;
+    }
+
+    public function insert_award_log($data) {
+        $this->psql->insert('ifi_award_log',$data);
+    }
+
+    public function get_signer_m_block($signer) {
+        $this->psql->select('MAX(number) as max_number, MIN(number) as min_number');
+        $this->psql->from('blocks');
+        $this->psql->where('miner',$signer);
+        $query = $this->psql->get();
+        return $query->row();
+    }
+
+    public function get_blocks_count_by_singer_time($start_time,$signer) {
+        $this->psql->select('number');
+        $this->psql->from('blocks');
+        $this->psql->where(['miner'=>$signer,'time >=' => $start_time]);
+        $query = $this->psql->get();
+        $count = $query->num_rows();
+        return $count;
+    }
+    
+    public function get_award_by_node_time($start_time,$node) {
+        $this->psql->select('SUM(ifi_amount) as sum_amount');
+        $this->psql->where(['node_address'=>$node,'timestamp >=' => $start_time]);
+        $res = $this->psql->get('ifi_award_log')->row();
+        return $res->sum_amount;
+    }
+    
+    public function get_incentive_reward($node) {
+        $this->psql->select('SUM(ifi_amount) as sum_amount');
+        $this->psql->where(['node_address'=>$node,'type' => 1]);
+        $res = $this->psql->get('ifi_award_log')->row();
+        return $res->sum_amount;
+    } 
+
+    public function update_node($data, $where) {
+        $this->psql->update('nodes',$data,$where);
+    }
+
+    public function update_block($data, $where) {
+        $this->psql->update('blocks',$data,$where);
+    }
+
+    public function update_config_vars($data,$where) {
+        $this->psql->update('config_vars',$data,$where);
+    }
+
+    public function set_node($data,$address) {
+        $this->psql->select('id');
+        $this->psql->from('nodes');
+        $this->psql->where('owner_address', $address);
+        $query = $this->psql->get();
+        $count = $query->num_rows();
+        if($count > 0) {
+            // just update it
+            $data['last_updated'] = date('Y-m-d H:i:s');
+            $this->psql->update('nodes', $data, array('owner_address'=>$address));
+        } else {
+            // insert it
+            $this->psql->insert('nodes', $data);
+        }
+    }
+
+    public function insert_node_startup($data) {
+        // just insert it
+        $this->psql->insert('nodes_startup',$data);
+    }
+
+    public function get_node_number() {
+        $this->psql->select('id');
+        $this->psql->from('nodes');
+        $this->psql->where('last_updated >', date('Y-m-d H:i:s', time()-24*3600));
+        $query = $this->psql->get();
+        $count = $query->num_rows();
+        return $count;
+    }
+
+    public function get_address_number() {
+        $this->psql->select('id');
+        $this->psql->from('nodes');
+        $query = $this->psql->get();
+        $count = $query->num_rows();
+        return $count;
+    }
+
+    public function get_tx_count() {
+        $this->psql->select('hash');
+        $this->psql->from('transactions');
+        $query = $this->psql->get();
+        $count = $query->num_rows();
+        return $count;
+    }
+
+    public function get_signers_count() {
+        $this->psql->select('id');
+        $this->psql->from('signers');
+        $query = $this->psql->get();
+        $count = $query->num_rows();
+        return $count;
+    }
+
+}
