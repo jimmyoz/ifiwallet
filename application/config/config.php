3f7e340337642c9366ce3ebb2ec0e0f9edebee27
root <root@ip-172-26-4-187.ap-northeast-1.compute.internal>
2022-06-15 07:17:17 +0000

    20220615

---
+++ application/config/config.php
@@ -0,0 +1,197 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['sault'] = "PAH535BJn/kfWkN9puk=";
+$config['readyTimeStamp'] = 1525104000;
+$config['cryptoKey'] = "dlsowlaofjslwkehfkslalfhgksoqpd";
+$config['cryptoIV'] = "abcdeflkdiwoieks";
+$config['loopTime'] = 30;
+$config['minAvailableAccounts'] = 1000;
+$config['rpcuser'] = "jameswalstonn";
+$config['rpcpass'] = "123456";
+$config['db_prefix'] = "fw_";
+// Bitcoin
+$config['btc_coin_id'] = 1;
+$config['btcPWD'] = "P@ssw0rd";
+$config['btcPort'] = "8332";
+$config['btcPayAccount'] = "jameswalstonn";
+$config['btcPayAddress'] = "mof16X3kk4nqscNRFRzGNNGiZ7CxSyQajq";
+$config['btcWithdrawTo'] = "";
+$config['FEE_PER_BYTE'] = "0.00000200";
+$config['FEE_PER_KILOBYTE'] = "0.00200000";
+// USDT OMNI
+$config['usdt_coin_id'] = 2;
+$config['usdtPWD'] = "P@ssw0rd";
+$config['usdtPort'] = "8332";
+$config['usdtPayAccount'] = "jameswalstonn";
+$config['usdtPayAddress'] = "mof16X3kk4nqscNRFRzGNNGiZ7CxSyQajq";
+$config['usdtWithdrawTo'] = "";
+$config['FEE_PER_BYTE'] = "0.00000200";
+$config['FEE_PER_KILOBYTE'] = "0.00200000";
+//Bitcoin Cash
+$config['bch_coin_id'] = 100011;
+$config['bchPWD'] = "P@ssw0rd";
+$config['bchPort'] = "18332";
+$config['bchPayAccount'] = "jameswalstonn";
+$config['bchPayAddress'] = "qqd26jgelu2vk2rkr4xff0w7d5x892exrvncxhfz2p";
+$config['bchWithdrawTo'] = "qqldv3lkmnkcn6hrdmgwaxss67qcrwme7qyzca20kk";
+$config['FEE_PER_BYTE'] = "0.00000200";
+$config['FEE_PER_KILOBYTE'] = "0.00200000";
+
+//DOGE
+$config['doge_coin_id'] = 100033;
+$config['dogePort'] = 44555;
+$config['dogePayAccount'] = "bgjio";
+$config['dogePayAddress'] = "njrEQ7NPB6Q6Y9YrEd8o4hvjGKZsGPguA1";
+$config['dogeWithdrawTo'] = "ncsPtJy7cBbekEhDUFh8yRXAurrzbFtnGH";
+
+//BTM
+$config['btm_coin_id'] = 4;
+$config['btmPWD'] = "123456";
+$config['btmReserveKeystore'] = "7b03f5d3-9bc3-4a2d-9fd6-c02af17142fb";
+$config['btmKeystorePath'] = "/home/hcf_app/btm/keystore";
+$config['btmPayAccountId'] = "0I66F515G0A0A";
+$config['btmPayAddress'] = "tm1qkfp4dqyrfzw0clelajyd6wjnwsfdmkklza5hqs";
+$config['btmWithdrawTo'] = "tm1qtjjg3zsvsuww4z60pkzmjqa7hhzelrg7q5pvvz";
+$config['btmEstimateFee'] = 50000000;
+
+
+//ONE
+$config['one_coin_id'] = 100031;
+$config['onePort'] = 8092;
+$config['onePWD'] = "123456";
+$config['oneAccount'] = "jameswalstonn1";
+$config['oneAccountId'] = "1.2.552123";
+
+//ZEN CASH
+$config['zen_coin_id'] = 100030;
+$config['zenPort'] = 18232;
+$config['zenPayAccount'] = "";
+$config['zenPayAddress'] = "ztqgBKEpDA4KB51EWtznkccu31bq4ApuFme";
+
+//DASH
+$config['dash_coin_id'] = 100027;
+$config['dashPort'] = 9998;
+$config['dashPWD'] = "123456";
+$config['dashPayAccount'] = "dash15337961600";
+$config['dashPayAddress'] = "ydezeB9tkLByAqEeZeb3KBd8UvDAa2nP6d";
+$config['dashWithdrawTo'] = "yWdXnYxGbouNoo8yMvcbZmZ3Gdp6BpySxL";
+
+//Ripple
+$config['xrp_coin_id'] = 5;
+$config['xrpPort'] = 5005;
+$config['xrp_hw_address'] = "rK56Pv6NYTYDKvW3VS74JhFVGLYj9y12iY";
+$config['xrp_hw_secret'] = "GvSufkCz5ZnzuUmEsP0G/0y76hucpKJ717+pM7FGNP4=";
+$config['xrp_fee'] = 0.001;
+
+//Monero
+$config['xmr_coin_id'] = 100016;
+$config['xmrPort'] = 28084;
+$config['xmrPWD'] = "123456";
+$config['xmrWalletFile'] = "wallet.json";
+$config['xmr_pay_account_index'] = 0;
+$config['xmr_tx_fee'] = 0.002;
+$config['xmrWithdrawTo'] = "BZtv24uyMc7FVDV4eJCDQBDRSsPnKrSX22sU4UW89ryeJk79QtqCyop6hPo3CCVNL1UaLgwD4BxWkPMG7bqVu37N1T58KQj";
+
+
+//Cardano
+$config['ada_coin_id'] = 21;
+$config['ada_phase'] = ["bins", "material", "silly", "twice", "direct", "slush", "pistol", "razor", "become", "junk", "kingdom", "flee"];
+$config['accountIndex'] = 3244823918;
+$config['wallet_id'] = 'Ae2tdPwUPEZAwHv8bQoy6ssH8odwJnKuf7X2fpHTqab7deWhwCBfPevKzAZ';
+$config['spendingPassword'] = '5416b2988745725998907addf4613c9b0764f04959030e1b81c603b920a115d0';
+$config['walletName'] = 'JamesWallet';
+$config['test_amount'] = 130400;
+$config['test_address'] = "DdzFFzCqrhsqqxuNhgafbPY8hgSAijZoZ4CaqSa9VbUvZhW7yHcgRxoBmQNS2QsEMGs2cPkkamSuGquJrsF5xxFre2mvShtTx2LkWiPX";
+
+
+//GXChain
+$config['gxs_coin_id'] = 100004;
+$config['gxsAccount'] = "jameswalstonn";
+$config['gxsAccountId'] = "1.2.237";
+$config['gxsPWD'] = "P@ssw0rd";
+$config['gxsPort'] = "8091";
+
+//EOS
+$config['eos_coin_id'] = 9;
+$config['eosAccount'] = "12345h12345h";
+$config['eosWallet'] = "default";
+$config['eosPWD'] = "PW5KB3Ug5tRDVSksGJj1bC9ys4Na2P3S7CEDQ58S8HPFBKuyNLKyR";
+
+/*
+|--------------------------------------------------------------------------
+| Base Site URL
+|--------------------------------------------------------------------------
+|
+| URL to your CodeIgniter root. Typically this will be your base URL,
+| WITH a trailing slash:
+|
+|       http://example.com/
+|
+| WARNING: You MUST set this value!
+|
+| If it is not set, then CodeIgniter will try guess the protocol and path
+| your installation, but due to security concerns the hostname will be set
+| to $_SERVER['SERVER_ADDR'] if available, or localhost otherwise.
+| The auto-detection mechanism exists only for convenience during
+| development and MUST NOT be used in production!
+|
+| If you need to allow multiple domains, remember that this file is still
+| a PHP script and you can easily do that on your own.
+|
+*/
+$config['base_url'] = 'http://localhost/scriptfullwallet/';
+
+
+$config['index_page'] = 'index.php';
+$config['uri_protocol'] = 'REQUEST_URI';
+$config['url_suffix'] = '';
+$config['language']     = 'english';
+$config['charset'] = 'UTF-8';
+$config['enable_hooks'] = FALSE;
+$config['subclass_prefix'] = 'MY_';
+$config['composer_autoload'] = FALSE;
+$config['permitted_uri_chars'] = '';
+$config['enable_query_strings'] = FALSE;
+$config['controller_trigger'] = 'c';
+$config['function_trigger'] = 'm';
+$config['directory_trigger'] = 'd';
+$config['allow_get_array'] = TRUE;
+$config['log_threshold'] = 0;
+$config['log_path'] = '';
+$config['log_file_extension'] = '';
+$config['log_file_permissions'] = 0644;
+$config['log_date_format'] = 'Y-m-d H:i:s';
+$config['error_views_path'] = '';
+$config['cache_path'] = '';
+$config['cache_query_string'] = FALSE;
+$config['encryption_key'] = '';
+$config['sess_driver'] = 'files';
+$config['sess_cookie_name'] = 'ci_session';
+$config['sess_expiration'] = 7200;
+$config['sess_save_path'] = NULL;
+$config['sess_match_ip'] = FALSE;
+$config['sess_time_to_update'] = 300;
+$config['sess_regenerate_destroy'] = FALSE;
+$config['cookie_prefix']        = '';
+$config['cookie_domain']        = '';
+$config['cookie_path']          = '/';
+$config['cookie_secure']        = FALSE;
+$config['cookie_httponly']      = FALSE;
+$config['standardize_newlines'] = FALSE;
+$config['global_xss_filtering'] = FALSE;
+$config['csrf_protection'] = FALSE;
+$config['csrf_token_name'] = 'csrf_test_name';
+$config['csrf_cookie_name'] = 'csrf_cookie_name';
+$config['csrf_expire'] = 7200;
+$config['csrf_regenerate'] = TRUE;
+$config['csrf_exclude_uris'] = array();
+$config['compress_output'] = FALSE;
+$config['time_reference'] = 'local';
+$config['rewrite_short_tags'] = FALSE;
+$config['proxy_ips'] = '';
+$config['time_reference'] = 'local';
+$config['rewrite_short_tags'] = FALSE;
+$config['proxy_ips'] = '';
+
+
